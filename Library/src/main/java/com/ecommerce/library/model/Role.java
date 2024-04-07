b2ae799257126ae290b606a16a53fcd378e700a9
quoc <lyhangquoc274@gmail.com>
2024-04-07 15:41:50 +0700

    admin config

---
+++ Library/src/main/java/com/ecommerce/library/model/Role.java
@@ -0,0 +1,20 @@
+package com.ecommerce.library.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private Long id;
+
+    private String name;
+}
